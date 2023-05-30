2f635de247ac73d6d0b13b58ad42cbd32c9ae4fe
Japan <2113338@mail.ru>
2023-05-30 09:42:54 +0300

    Навигация по сайту при помощи JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// heder backgroutd start
 (function () {
     const header = document.querySelector('.header');
     window.onscroll = () => {
@@ -8,7 +9,10 @@
         }
     }
 }());
+// header background end
 
+
+// burger menu start
 (function () {
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header__nav');
@@ -20,3 +24,46 @@
         menu.classList.remove('header__nav--active');
     });
 }());
+// burger menu end
+
+
+// Scroll to anchors strart
+(function () {
+
+    const smoothScroll = function (targetEl, duration) {
+        const headerElHeight =  document.querySelector('.header').clientHeight;
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
+    
+        const ease = function(t,b,c,d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+    
+        const animation = function(currentTime){
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0,run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+
+    };
+
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
+}());
+// Scroll to anchors end
